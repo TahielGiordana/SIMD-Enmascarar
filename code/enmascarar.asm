3097694a29a3803d4d14ca81062f498ec083de08
lapassesungs <tahielgiordana@gmail.com>
2021-05-30 18:56:53 -0300

    Terminada la funcion asm

--- code/enmascarar.asm
+++ code/enmascarar.asm
@@ -1,5 +1,5 @@
 section .data
-    num dw 20
+    num dq 0
 
 section .text
 
@@ -13,7 +13,8 @@ enmascarar_asm:
     xor ecx,ecx
 
     mov eax,[ebp+8]     ;Almacena la direccion del buffer1
-    mov ebx,[ebp+16]    ;Almacena la direccion de la mascara
+    mov ebx,[ebp+12]    ;Almacena la direccion del buffer2
+    mov edx,[ebp+16]    ;Almacena la direccion de la mascara
     mov ecx,[ebp+20]    ;Almacena el size
     
     jmp loop
@@ -21,15 +22,22 @@ enmascarar_asm:
 loop:
 
     cmp ecx,0           ;Revisar que queden pixeles por recorrer
-    je end              ;Sino terminar la funcion
+    jl end              ;Sino terminar la funcion
 
-    movq mm0,qword[eax] ;Guardar en el registro mm0 los primeros 8 caracteres de eax
-    movq mm1,qword[ebx] ;Guardar en el registro mm1 los primeros 8 caracteres de ebx
-    pxor mm0,mm1        ;Hace un xor entre los bytes de la img1 y la mascara
-    movq [eax],mm0      ;Mover al registro eax los caracteres modificados
+    movq mm0,qword[eax] ;Guardar en el registro mm0 los primeros 8 caracteres del buffer1
+    movq mm1,qword[ebx] ;Guardar en el registro mm1 los primeros 8 caracteres del buffer2
+    movq mm2,qword[edx] ;Guardar en el registro mm1 los primeros 8 caracteres de la mascara
+
+    pand mm1,mm2
+
+    pandn mm2,mm0
+    por mm1,mm2
+
+    movq [eax],mm1      ;Mover al registro eax los caracteres modificados
   
     add eax,8           ;mover eax 8 bytes
     add ebx,8
+    add edx,8
     sub ecx,8           ;decremento ecx en 8. (size/8)
     jmp loop
 
